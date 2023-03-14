31d5d337ec899fd6072dea2eeac5e14fb85120fe
Olena Pelagenko <84686704+ljenchik@users.noreply.github.com>
2023-03-14 23:13:57 +0000

    Added cards to project gallery

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,38 +1,27 @@
-import { useState } from "react";
 import "./Card.css";
 
-const Card = () => {
+const Card = (props) => {
   return (
     <div className="card-container">
       <div className="flip-card">
         <div className="flip-card-inner">
           <div className="flip-card-front">
-            <img src="./images/my-mars-mission.png" className="card-image" />
+            <img src={props.image} className="card-image" />
           </div>
           <div className="flip-card-back">
-            <h1>My Mars Mission</h1>
+            <h1>{props.title}</h1>
             <p>
-              A website for teenagers about Mars, which displays images of Mars
-              from the NASA API. It has a multiple answer quiz, and creates and
-              updates the user’s account, which stores and creates tickets to
-              Mars
-            </p>
-            <p>Technologies used </p>
-            <p>
-              JavaScript, React, Node.js, PostgreSQL, CSS, SQL, Bcrypt, Rest
-              Api, Font Awesome
+              {props.description}
             </p>
             <p>
-              <i class="fa-brands fa-github"></i>{" "}
-              <a href="https://github.com/ljenchik/my-mars-mission">
-                https://github.com/ljenchik/my-mars-mission
-              </a>
+             {props.technologies}
+            </p>
+            <p>
+              {props.github}
             </p>
             <p>
               Site is live at{" "}
-              <a href="https://ljenchik.github.io/my-mars-mission/">
-                https://ljenchik.github.io/my-mars-mission/
-              </a>
+              {props.live}
             </p>
           </div>
         </div>
--- src/pages/ProjectGallery.js
+++ src/pages/ProjectGallery.js
@@ -1,16 +1,61 @@
 import "./ProjectGallery.css";
 import Card from "../components/Card/Card";
 
-const projects = [];
-
+const projects = [
+  {
+    title: "My Mars Mission",
+    image: "./images/my-mars-mission.png",
+    description:
+      "A website for teenagers about Mars, which displays images of Mars from the NASA API. It has a multiple answer quiz, and creates and updates the user’s account, which stores and creates tickets to Mars",
+    technologies:
+      "JavaScript, React, Node.js, PostgreSQL, CSS, SQL, Bcrypt, Rest Api, Font Awesome",
+    github: "https://github.com/ljenchik/my-mars-mission",
+    live: "https://ljenchik.github.io/my-mars-mission",
+  },
+  {
+    title: "Weather Dashbpoard",
+    image: "./images/weather.png",
+    description:
+      ">A weather forecast application which displays current weather and 5-day forecast of the chosen city",
+    technologies: "JavaScript, Open Weather API",
+    github: "https://github.com/ljenchik/weather-dashboard",
+    live: "https://ljenchik.github.io/weather-dashboard/",
+  },
+  {
+    title: "Space Exploring",
+    image: "./images/space-exploring.png",
+    description:
+      "Welcome to Space Exploring! It's a website about space for children and teenagers. The website allows users to explore interesting facts about the solar system, view space images taken by Mars rovers, and score in the solar system quiz",
+    technologies:
+      "JavaScript,Bootstrap, CSS, jQuery, Font Awesome, NASA Open API, The Solar System OpenData",
+    github: "https://github.com/ljenchik/space-exploring",
+    live: "https://ljenchik.github.io/space-exploring/",
+  },
+  {
+    title: "School Departments",
+    image: "./images/school-departments.jpeg",
+    description:
+      "This application displays a list of school departments, the number of employees and the average salary for each of these departments. It displays a list of employees, searches employees born on a specific date or in the period between two given dates as well as adds, edits or deletes departments and employees",
+    technologies: "Typescript, Node.js, React, PostgreSQL, Bootstrap, CSS, SQL",
+    github: "https://github.com/ljenchik/school-departments-ts",
+    live: "",
+  },
+];
 
 const ProjectGallery = () => {
   return (
-    <div className="projects-container">
-        <h1 className="projects-title">Projects</h1>
-        <Card />
-    </div>
+    <><h1 className="projects-title">Projects</h1><div className="projects-container">
+      {projects.map((project) => (
+        <Card
+          title={project.title}
+          image={project.image}
+          description={project.description}
+          technologies={project.technologies}
+          github={project.github}
+          live={project.live} />
+      ))}
+    </div></>
   );
-}
+};
 
 export default ProjectGallery;
